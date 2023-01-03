fddc259794c4b6ed1c99e8b6e8e4b9ce892cfb84
atabul <atabulnadaf386@gmail.com>
2023-01-03 15:33:44 +0545

    some change

---
+++ FifthDay.py
@@ -0,0 +1,95 @@
+#Polymorphism
+'''class Cat:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def info(self):
+        print(f"I am a cat. My name is {self.name}. I am {self.age} years old.")
+    def make_sound(self):
+        print("Meow")
+
+class Dog:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def info(self):
+        print(f"I am a dog. My name is {self.name}. I am {self.age} years old")
+    def make_sound(self):
+        print("Bark")
+
+cat1=Cat("Kitty",2.5)
+dog1=Dog("Rocky",4.8)
+
+for animal in (cat1,dog1):
+    animal.make_sound()
+    animal.info()
+    animal.make_sound()''' 
+
+
+#Incapsulation(Call one function then other can be hidden) and Abstraction(Keep all the function in the different modules)
+'''class Person:
+    def __init__(self):
+        self.name="Atabul"
+    def bio(self):
+        self.addr="Bakers Street , London"
+        self.taxInfo="HUAPK29971"
+        self.contact="01-777-523-342"
+        print(self.addr, self.taxInfo, self.contact)
+    def interest(self):
+        self.favFood="Chinese"
+        self.hobbies="Python Programming"
+        self.bloodGroup="A+"
+        print(self.favFood, self.hobbies, self.bloodGroup)
+
+obj=Person()
+print(obj.name)
+obj.bio()
+obj.interest()''' 
+
+'''class Library:
+    def __init__(self,books):
+        self.books=books
+    def list_books(self):
+        print("Available Books")
+        for book in self.books:
+            print(book)
+    def borrrow_book(self,borrrow_book):
+        if borrrow_book in self.books:
+            print("Get your book Now")
+            self.books.remove(borrrow_book)
+        else:
+            print("Book not Available")
+
+    def receive_book(self,receive_book):
+        print("You returned the Book")
+        self.books.append(receive_book)
+books=['C','C++','Java']
+O=Library(books)
+
+msg="""
+1. Display Book 
+2. Borrow Book
+3. Return Book
+"""
+
+while True:
+    print(msg)
+    ch=int(input("Enter your choice: "))
+    if ch==1:
+        O.list_books()
+    elif ch==2:
+        book=input("Enter book name to borrow:")
+        O.borrow_book(book)
+    elif ch==3:
+        book=input("Enter book name to return:")
+        O.receive_book(book)
+    else:
+        print("Thank you come again")
+        quite()''' 
+
+      #Web Application  
+
+
+
+
+
---
+++ FourthDay.py
@@ -0,0 +1,121 @@
+#iterate the given number list of numbers abd print only those numbers which are divisible by 5. Given list is [10,20, 33,46,55]
+'''l=[]
+l.append(10)
+l.append(20)
+l.append(33)
+l.append(46)
+l.append(55)
+myList={x%5 for x in [10,20,33,46,55]}
+print(myList)''' 
+
+#Print the following pattern
+'''1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5'''
+
+'''for i in range(1,6):
+    for j in range(i):
+        print(i,end=' ')
+    print()'''
+
+
+#write a program to check if the given number is a palindrome number
+'''def palindrome(num):
+    temp=num
+    rev=0
+    while(num>0):
+        dig=num%10
+        rev=rev*10+dig
+        num=num/10
+    if(temp==rev):
+        print("The number is palindrome !")
+    else:
+        print("Not a palindrome!")
+
+num=int(input("Enter a number:"))
+palindrome(num)'''
+
+#Class
+'''class Person:
+    def __init__(self, name, age):       #init and str are initialization and structure function and these are standard for all class also take double dash before and after.
+        self.name=name
+        self.age=age
+    def __str__(self):
+        return f"{self.name}({self.age})"
+    def myfunc(self):
+        print("The given name is ",self.name)
+p1 = Person("Atabul",25)
+p2=Person("Raj",23)
+print(p1.name)
+print(p1.age)
+print(p1)
+print(p2)
+print(p1.myfunc())'''
+
+
+'''class Room:
+    length=0.0
+    breadth=0.0
+    def calculate_area(self):
+        print("Area of Room =",self.length*self.breadth)
+study_room=Room()
+study_room.length=42.5
+study_room.breadth=30.8
+study_room.calculate_area()'''
+
+#inheritance
+'''class Animal:
+    name=""
+    def eat(self):
+        print("I can eat")
+class Dog(Animal):
+    def display(self):
+        print("My name is ",self.name)
+labrador=Dog()
+labrador.name="Atabul"
+labrador.eat()
+labrador.display()'''
+
+'''class Car:
+    name=""
+    def drive(self):
+        print("Drive slow!")
+class Hundai(Car):
+    def display(self):
+        print("The best company in the world",self.name)
+abc=Hundai()
+abc.name="TVS"
+abc.drive()
+abc.display()'''
+
+
+#Super Method
+class Animal:
+    name=""
+    def eat(self):
+        print("i can eat")
+class Dog(Animal):
+    def eat(self):
+        super().eat()       #Call super class using super function
+        print("I like to eat bones")
+laborador=Dog()
+laborador.eat()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
